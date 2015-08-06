4cee031c699bb1443b35757aac9d6d2798311952
Máiréad <magical.magical.me@gmail.com>
2015-08-06 18:04:43 -0500

    Has a more correct robot map.

--- src/org/usfirst/frc/team4536/robot/RobotMap.java
+++ src/org/usfirst/frc/team4536/robot/RobotMap.java
@@ -7,19 +7,19 @@ package org.usfirst.frc.team4536.robot;
  */
 public class RobotMap {
 	/*-------------------------------------Motor Channels------------------------------------*/	
-    public static final int LEFT_MOTOR = 0; //needs to be checked
-    public static final int RIGHT_MOTOR = 1;
-    public static final int FRONT_ROLLER_MOTOR = 2;
-    public static final int BACK_ROLLER_MOTOR = 3;
+    public static final int LEFT_MOTOR = 1;
+    public static final int RIGHT_MOTOR = 0;
+    public static final int FRONT_ROLLER_MOTOR = 3;
+    public static final int BACK_ROLLER_MOTOR = 2;
     
     /*-------------------------------------Joystick Ports---------------------------------*/
     public static final int MAIN_STICK = 0; // (Also the left stick for tank drive)
     public static final int SECONDARY_STICK = 1; // (Also the right stick for tank drive)
     
     /*-------------------------------------Solenoid Channels------------------------------------*/	
-    public static final int FRONT_ARM_RIGHT_SOLENOID_CHANNEL = 2;
-	public static final int FRONT_ARM_LEFT_SOLENOID_CHANNEL = 3;
+    public static final int FRONT_ARM_RIGHT_SOLENOID_CHANNEL = 5;
+	public static final int FRONT_ARM_LEFT_SOLENOID_CHANNEL = 6;
 	public static final int BACK_ARM_RIGHT_SOLENOID_CHANNEL = 4;
-	public static final int BACK_ARM_LEFT_SOLENOID_CHANNEL = 5;
+	public static final int BACK_ARM_LEFT_SOLENOID_CHANNEL = 7;
 }
 
